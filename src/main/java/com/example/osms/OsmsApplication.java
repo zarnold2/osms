d0bd7396bdf0ab943b7f253608b976d99c1a20c4
Zach <zacharyarnold2000@yahoo.com>
2019-11-18 20:07:07 -0600

    add eureka hooks

--- src/main/java/com/example/osms/OsmsApplication.java
+++ src/main/java/com/example/osms/OsmsApplication.java
@@ -5,9 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @RestController
+@EnableDiscoveryClient
 public class OsmsApplication {
 
 	public static void main(String[] args) {
