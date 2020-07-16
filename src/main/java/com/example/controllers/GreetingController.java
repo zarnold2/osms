6c5c31a0bc201616287de3a9cadbf6ab78a792c3
Zach <zacharyarnold2000@yahoo.com>
2020-07-15 21:55:41 -0500

    add new directories

--- src/main/java/com/example/controllers/GreetingController.java
+++ src/main/java/com/example/controllers/GreetingController.java
@@ -1,18 +1,24 @@
-package com.example.osms;
+package com.example.controllers;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-@RestController
-public class OsmsApplication {
+import com.example.services.GreetingService;
 
-	public static void main(String[] args) {
-		SpringApplication.run(OsmsApplication.class, args);
+@RestController
+public class GreetingController {
+  
+  @Autowired
+  GreetingService greetingServices;
+  
+  /*
+  static void main(String[] args) {
+		SpringApplication.run(GreetingController.class, args);
 	}
+	*/
 	
   @GetMapping(value="/")
   public String greeting()
