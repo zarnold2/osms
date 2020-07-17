7d7b5b79a8291915becb7cbb9596ea4e79c04bdb
Zach <zacharyarnold2000@yahoo.com>
2020-07-16 21:53:26 -0500

    fix controller

---
+++ src/main/java/com/example/OsmsApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsmsApplication.class, args);
+	}
+
+}
